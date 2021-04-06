148af5b2a39aeede9c9ece96e65ae0e3bb1f754d
santiago padilla <santiagopadilla117@gamil.com>
2021-04-05 19:26:59 -0500

    second commit -index + -new views

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,69 @@
-
+// button responsive click
 $(document).ready(function () {
-    $(".nav-toggler").each(function (_, navToggler) {
-      var target = $(navToggler).data("target");
-      $(navToggler).on("click", function () {
-        $(target).animate({
-          height: "toggle",
-        });
+  $(".nav-toggler").each(function (_, navToggler) {
+    var target = $(navToggler).data("target");
+    $(navToggler).on("click", function () {
+      $(target).animate({
+        height: "toggle",
       });
     });
   });
+});
+
+//Carrousel-slider
+let cont = 0;
+function loopSlider() {
+  let xx = setInterval(function () {
+    switch (cont) {
+      case 0: {
+        $("#slider-1").fadeOut(400);
+        $("#slider-2").delay(400).fadeIn(400);
+        $("#sButton1").removeClass("bg-purple-800");
+        $("#sButton2").addClass("bg-purple-800");
+        cont = 1;
+
+        break;
+      }
+      case 1: {
+        $("#slider-2").fadeOut(400);
+        $("#slider-1").delay(400).fadeIn(400);
+        $("#sButton2").removeClass("bg-purple-800");
+        $("#sButton1").addClass("bg-purple-800");
+
+        cont = 0;
+
+        break;
+      }
+    }
+  }, 8000);
+}
+
+function reinitLoop(time) {
+  clearInterval(xx);
+  setTimeout(loopSlider(), time);
+}
+
+function sliderButton1() {
+  $("#slider-2").fadeOut(400);
+  $("#slider-1").delay(400).fadeIn(400);
+  $("#sButton2").removeClass("bg-purple-800");
+  $("#sButton1").addClass("bg-purple-800");
+  reinitLoop(4000);
+  cont = 0;
+}
+
+function sliderButton2() {
+  $("#slider-1").fadeOut(400);
+  $("#slider-2").delay(400).fadeIn(400);
+  $("#sButton1").removeClass("bg-purple-800");
+  $("#sButton2").addClass("bg-purple-800");
+  reinitLoop(4000);
+  cont = 1;
+}
+
+$(window).ready(function () {
+  $("#slider-2").hide();
+  $("#sButton1").addClass("bg-purple-800");
+
+  loopSlider();
+});
